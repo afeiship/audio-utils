e3885e21f4e3377bc54df1a810085c3f5b4b2732
afeiship <1290657123@qq.com>
2017-03-07 09:21:42 +0800

    naiming

--- src/components/common/home.vue
+++ src/components/common/home.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script>
-    import vHead from './Header.vue';
-    import vSidebar from './Sidebar.vue';
+    import vHead from './header.vue';
+    import vSidebar from './sidebar.vue';
     export default {
         components:{
             vHead, vSidebar
