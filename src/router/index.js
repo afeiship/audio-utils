b851a6a69deecdbec51d66ca16274b3959c6e48a
afeiship <1290657123@qq.com>
2017-05-04 22:25:29 +0800

    udpate dist/readme

--- src/router/index.js
+++ src/router/index.js
@@ -11,49 +11,49 @@ export default new Router({
         },
         {
             path: '/readme',
-            component: resolve => require(['../components/common/Home.vue'], resolve),
+            component: resolve => require(['../components/common/home.vue'], resolve),
             children:[
                 {
                     path: '/',
-                    component: resolve => require(['../components/page/Readme.vue'], resolve)
+                    component: resolve => require(['../components/page/readme.vue'], resolve)
                 },
                 {
                     path: '/basetable',
-                    component: resolve => require(['../components/page/BaseTable.vue'], resolve)
+                    component: resolve => require(['../components/page/base-table.vue'], resolve)
                 },
                 {
                     path: '/vuetable',
-                    component: resolve => require(['../components/page/VueTable.vue'], resolve)     // vue-datasource组件
+                    component: resolve => require(['../components/page/vue-table.vue'], resolve)     // vue-datasource组件
                 },
                 {
                     path: '/baseform',
-                    component: resolve => require(['../components/page/BaseForm.vue'], resolve)
+                    component: resolve => require(['../components/page/base-form.vue'], resolve)
                 },
                 {
                     path: '/vueeditor',
-                    component: resolve => require(['../components/page/VueEditor.vue'], resolve)    // Vue-Quill-Editor组件
+                    component: resolve => require(['../components/page/vue-editor.vue'], resolve)    // Vue-Quill-Editor组件
                 },
                 {
                     path: '/markdown',
-                    component: resolve => require(['../components/page/Markdown.vue'], resolve)     // Vue-Quill-Editor组件
+                    component: resolve => require(['../components/page/markdown.vue'], resolve)     // Vue-Quill-Editor组件
                 },
                 {
                     path: '/upload',
-                    component: resolve => require(['../components/page/Upload.vue'], resolve)       // Vue-Core-Image-Upload组件
+                    component: resolve => require(['../components/page/upload.vue'], resolve)       // Vue-Core-Image-Upload组件
                 },
                 {
                     path: '/basecharts',
-                    component: resolve => require(['../components/page/BaseCharts.vue'], resolve)   // vue-echarts-v3组件
+                    component: resolve => require(['../components/page/base-charts.vue'], resolve)   // vue-echarts-v3组件
                 },
                 {
                     path: '/mixcharts',
-                    component: resolve => require(['../components/page/MixCharts.vue'], resolve)    // vue-echarts-v3组件
+                    component: resolve => require(['../components/page/mix-charts.vue'], resolve)    // vue-echarts-v3组件
                 }
             ]
         },
         {
             path: '/login',
-            component: resolve => require(['../components/page/Login.vue'], resolve)
+            component: resolve => require(['../components/page/login.vue'], resolve)
         },
     ]
 })
