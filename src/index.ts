133630f76037f13bd1392f99354b36b6e538a89b
afeiship <1290657123@qq.com>
2023-12-13 16:27:49 +0800

    feat: upt recorder/howler impl

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import watchAmplitude from './lib/watch-amplitude';
-import watchHowlerAmplitude from './lib/watch-howler-amplitude';
 import checkPermission from './lib/check-permission';
 
-export { watchAmplitude, watchHowlerAmplitude, checkPermission };
+export { watchAmplitude, checkPermission };
--- src/lib/watch-amplitude.ts
+++ src/lib/watch-amplitude.ts
@@ -1,28 +1,72 @@
-interface ChangeHandler {
-  (amplitude: number): void;
-}
-
 interface WatchOptions {
+  context?: any;
+  type?: 'recorder' | 'howler';
   interval?: number;
+
+  callback: (amplitude: number) => void;
 }
 
 const defaults: WatchOptions = {
+  context: null,
+  type: 'recorder',
   interval: 500,
+  callback: (_) => {
+  },
 };
 
-export default function watchAmplitude(
-  inBufferGetter: any,
-  inCallback: ChangeHandler,
-  inOptions?: WatchOptions
-) {
+class ProcessorRecorder {
+  protected dataArray;
+
+  constructor(public context) {
+  }
+
+  init() {
+  }
+
+  get() {
+    this.dataArray = this.context.getPlayAnalyseData();
+    const maxAmplitude = Math.max(...this.dataArray);
+    const diff = maxAmplitude - 128;
+    return diff / 128;
+  }
+}
+
+class ProcessorHowler {
+  protected dataArray;
+  protected analyser;
+
+  constructor(public context) {
+    this.dataArray = [];
+  }
+
+  init() {
+    const audioContext = this.context.ctx;
+    this.analyser = audioContext.createAnalyser();
+    this.context.masterGain.connect(this.analyser);
+    this.dataArray = new Uint8Array(this.analyser.frequencyBinCount);
+  }
+
+  get() {
+    this.analyser.getByteFrequencyData(this.dataArray);
+    const sum = this.dataArray.reduce(function(acc, val) {
+      return acc + val;
+    }, 0);
+    return sum / this.dataArray.length / 100;
+  }
+}
+
+export default function watchAmplitude(inOptions?: WatchOptions) {
   const options = { ...defaults, ...inOptions };
+  const { type } = options;
+  const Processor = type === 'recorder' ? ProcessorRecorder : ProcessorHowler;
+  const processor = new Processor(options.context);
+
+  processor.init();
+
   function calculate() {
     try {
-      const dataArray = inBufferGetter();
-      const maxAmplitude = Math.max(...dataArray);
-      const diff = maxAmplitude - 128;
-      const amplitude = diff / 128;
-      inCallback(amplitude);
+      const amplitude = processor.get();
+      options.callback(amplitude);
     } catch (error) {
       console.warn('Buffer data get failed~');
     }
@@ -32,7 +76,7 @@ export default function watchAmplitude(
 
   return {
     destroy() {
-      inCallback(0);
+      options.callback(0);
       clearInterval(timer);
     },
   };
